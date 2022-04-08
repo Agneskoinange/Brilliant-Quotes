57bd2f8d69ab873b0d791b5afe81e535d1ce27b6
Agnes <koinageagness@gmail.com>
2022-04-09 01:38:03 +0300

    add content directive highlight

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { Quote } from '../quote';
 
 
@@ -10,13 +10,16 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   Quotes:Quote [] = [
-    new Quote(1, "First Quote", "author", 0, 0),
-    new Quote(2, "First Quote", "author", 0, 0),
+    new Quote(1, "First Quote", "author", 2, 0),
+    new Quote(2, "First Quote", "author", 3, 0),
     new Quote(3, "First Quote", "author", 0, 0),
     new Quote(4, "First Quote", "author", 0, 0),
     new Quote(5, "First Quote", "author", 0, 0),
     new Quote(6, "First Quote", "author", 0, 0),
   ];
+
+  arr: number[] = this.Quotes.map(Quote=>Quote.upvotes)
+  highest=Math.max(...this.arr)
 
   constructor() { }
 
