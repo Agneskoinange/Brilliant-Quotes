34bb224c5690ad46d77a801c97352b120f474ccb
Agnes <koinageagness@gmail.com>
2022-04-08 22:32:56 +0300

    add html boiler plate to quote.component.html

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,17 @@
 export class Quote {
+    
+    showDescription: boolean;
+
+  constructor(
+
+    public id: number,
+    public quote: string,
+    public author: string,  
+    public upvotes: number, 
+    public downvotes: number, )
+
+    {
+
+    this.showDescription=false;
+  }
 }
