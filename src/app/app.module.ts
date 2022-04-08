ab8916663543fab8828199b10ad5c1d8d50956e4
Agnes <koinageagness@gmail.com>
2022-04-08 20:28:08 +0300

    delete quotes and add quote component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuoteComponent
   ],
   imports: [
     BrowserModule,
